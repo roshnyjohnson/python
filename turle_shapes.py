7c7228fb23598dae9fcc26e1c68b3057d671aab2
ROSHNY JOHNSON <roshnyjn05@gmail.com>
2025-06-11 14:02:37 +0530

    Create turle_shapes.py

---
+++ turle_shapes.py
@@ -0,0 +1,13 @@
+import random
+from turtle import Turtle, Screen
+
+# Create turtle object
+tom = Turtle()
+
+# List of colors
+colors = ["red", "green", "pink", "orange", "blue", "gray", "alice blue",
+          "aquamarine", "brown", "burlywood", "chocolate1", "cyan", "beige"]
+
+# Loop through number of sides from 3 (triangle) to 8 (octagon)
+for i in range(3, 9):
+    a
