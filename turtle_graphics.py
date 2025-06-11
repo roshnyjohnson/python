1dbb7c59c7fdb637e5ccd8588f5192d7a01daed3
ROSHNY JOHNSON <roshnyjn05@gmail.com>
2025-06-11 13:17:15 +0530

    Create turtle_graphics.py

---
+++ turtle_graphics.py
@@ -0,0 +1,14 @@
+import turtle#documentation on turtlegraphics in google
+turtle.getscreen()#not necessary
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(70.71)
+turtle.left(90)
+turtle.forward(70.71)
+turtle.shape("turtle")
+print(turtle.shape())
+turtle.exitonclick()
